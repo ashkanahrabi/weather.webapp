a8d76d5bb9425b036457dd180a24686c9d45c9bd
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-17 14:58:23 +0430

    init CardSlider

---
+++ src/components/cardSlider/CardSlider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cardSlider.scss';
+import CardItem from "../cardItem/CardItem";
+
+const CardSlider = () => {
+    return (
+        <div className={`card-slider-container`}>
+            <CardItem />
+
+        </div>
+    );
+};
+
+export default CardSlider;
