5807e33b5fa65710ef602aa8435a1cfc7f8d1738
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-18 14:14:33 +0430

    dynamic data from api and context + active item bug fix

--- src/components/cardItem/CardItem.js
+++ src/components/cardItem/CardItem.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './cardItem.scss';
+import {toCelsius} from "../../utils/numberMethods/toCelsius";
+import WeatherIcon from "../weatherIcon/WeatherIcon";
+import {SelectedTimeSlotContext} from "../../utils/storage/selectedTimeSlot";
 
-const CardItem = () => {
+const CardItem = ({data}) => {
+    const [selectedTime, setSelectedTime] = useContext(SelectedTimeSlotContext);
+    const localeOptions = {
+        hour: '2-digit',
+        minute:'2-digit'
+    };
+    const time = new Date(data.dt * 1000).toLocaleTimeString('en-GB', localeOptions);
+    const temp = toCelsius(data.main.temp);
     return (
-        <div className={`card-item`}>
-            <div className="text-secondary text-small">11:00</div>
-            <div className="card-item-icon"></div>
-            <div className="text-primary text-medium">20</div>
+        <div className={`card-item ${selectedTime?.dt === data.dt && `item-active`}`} onClick={() => setSelectedTime(data)}>
+            <div className="text-secondary text-small">{time}</div>
+            <div className="card-item-icon">
+                <WeatherIcon id={data.weather[0].icon}/>
+            </div>
+            <div className="text-primary text-medium">{temp}</div>
         </div>
     );
 };
