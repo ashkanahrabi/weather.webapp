d3daa2f00a1e71e41f41f622bd1efed2133cf407
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-19 03:04:42 +0430

    testid + null check

--- src/components/cardItem/CardItem.js
+++ src/components/cardItem/CardItem.js
@@ -1,24 +1,24 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import './cardItem.scss';
 import {toCelsius} from "../../utils/numberMethods/toCelsius";
 import WeatherIcon from "../weatherIcon/WeatherIcon";
-import {SelectedTimeSlotContext} from "../../utils/storage/selectedTimeSlot";
+import {useSelectedTimeContext} from "../../utils/storage/selectedTimeSlot";
 
 const CardItem = ({data}) => {
-    const [selectedTime, setSelectedTime] = useContext(SelectedTimeSlotContext);
+    const [selectedTime, setSelectedTime] = useSelectedTimeContext();
     const localeOptions = {
         hour: '2-digit',
         minute:'2-digit'
     };
-    const time = new Date(data.dt * 1000).toLocaleTimeString('en-GB', localeOptions);
-    const temp = toCelsius(data.main.temp);
+    const time = new Date(data?.dt * 1000).toLocaleTimeString('en-GB', localeOptions);
+    const temp = toCelsius(data?.main.temp);
     return (
-        <div className={`card-item ${selectedTime?.dt === data.dt && `item-active`}`} onClick={() => setSelectedTime(data)}>
-            <div className="text-secondary text-small">{time}</div>
+        <div className={`card-item ${selectedTime?.dt === data?.dt && `item-active`}`} onClick={() => setSelectedTime(data)} data-testid={'card-item'}>
+            <div className="text-secondary text-small" data-testid={'time'}>{time}</div>
             <div className="card-item-icon">
-                <WeatherIcon id={data.weather[0].icon}/>
+                <WeatherIcon id={data?.weather[0].icon}/>
             </div>
-            <div className="text-primary text-medium">{temp}</div>
+            <div className="text-primary text-medium" data-testid={'celsius-temp'}>{temp}</div>
         </div>
     );
 };
