8cc3b6537bce5f5e4d3a1382712ee39c4c293696
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-18 14:16:52 +0430

    handle api call + add first list item to selected context

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,41 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useContext, useEffect} from 'react';
 import './home.scss';
 import Header from "../../components/header/Header";
 import CardSlider from "../../components/cardSlider/CardSlider";
+import {useDataFetch} from "../../utils/customHooks/useDataFetch";
+import {SelectedTimeSlotContext} from "../../utils/storage/selectedTimeSlot";
 
 const Home = () => {
-    const [weatherData, setWeatherData] = useState();
+    const [selectedTime, setSelectedTime] = useContext(SelectedTimeSlotContext);
+    const apiParams = {
+        method: 'GET',
+        url: '/api'
+    };
+    const [apiResponse] = useDataFetch(apiParams);
     useEffect(() => {
-
-    }, []);
+        if(apiResponse.data && !apiResponse.isError){
+            // const currentTimestamp = Date.now() / 1000;
+            // let selectedItem = null;
+            // apiResponse.data.list.forEach(item => {
+            //     if(item.dt < currentTimestamp){
+            //         selectedItem = item;
+            //     }
+            // });
+            setSelectedTime(apiResponse.data.list[0])
+        }
+    }, [apiResponse, setSelectedTime]);
     return (
         <div className={`home-container`}>
-            <Header />
-            <CardSlider />
+            {
+                apiResponse.isLoading ?
+                    <div className="w-100 h-100 d-flex justify-content-center align-items-center text-primary text-large">Loading...</div> :
+                    apiResponse.data ?
+                        <Fragment>
+                            <Header city={apiResponse.data.city}/>
+                            <CardSlider cardItem={apiResponse.data.list}/>
+                        </Fragment> :
+                        ''
+            }
         </div>
     );
 };
