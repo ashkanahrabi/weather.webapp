0e0afbb915382feaf46de47520772663157596cf
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-18 14:18:18 +0430

    dynamic values from context and api

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './header.scss';
+import {SelectedTimeSlotContext} from "../../utils/storage/selectedTimeSlot";
+import {toCelsius} from "../../utils/numberMethods/toCelsius";
+import WeatherIcon from "../weatherIcon/WeatherIcon";
 
-const Header = () => {
+const Header = ({city}) => {
+    const [selectedTime] = useContext(SelectedTimeSlotContext);
+    const weekday = new Date(selectedTime?.dt * 1000).toLocaleDateString('default', {weekday: 'long'});
+    const dayOfMonth = new Date(selectedTime?.dt * 1000).toLocaleDateString('default', {day: '2-digit'});
+    const month = new Date(selectedTime?.dt * 1000).toLocaleDateString('default', {month: 'long'});
     return (
         <div className={`header-container`}>
-            <div className={`header-icon`}></div>
+            <div className={`header-icon`}>
+                <WeatherIcon id={selectedTime.weather[0].icon}/>
+            </div>
             <div className={`header-temp-container align-items-center`}>
                 <div className={`w-100 d-flex justify-content-between`}>
-                    <div className="text-secondary text-small">cloudy</div>
-                    <div className="text-secondary text-small">12 / 2</div>
+                    <div className="text-secondary text-small">{selectedTime?.weather[0].main}</div>
+                    <div className="text-secondary text-small">{toCelsius(selectedTime?.main.temp_max)} / {toCelsius(selectedTime?.main.temp_min)}</div>
                 </div>
-                <div className={`text-primary text-extra-large`}>7</div>
+                <div className={`text-primary text-extra-large`}>{toCelsius(selectedTime?.main.temp)}</div>
             </div>
             <div className={`header-date-container`}>
-                <div className="text-secondary text-small">cloudy</div>
-                <div className="text-primary text-medium">cloudy</div>
-                <div className="text-primary text-medium">cloudy</div>
+                <div className="text-secondary text-small">{city.name}</div>
+                <div className="text-primary text-medium">{weekday}</div>
+                <div className="text-primary text-medium">{`${dayOfMonth}. ${month}`}</div>
             </div>
         </div>
     );
